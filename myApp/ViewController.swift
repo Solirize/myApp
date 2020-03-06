f68d1e108d4858b06d3bda95bf0b48498f1a5567
Solirize <41306882+Solirize@users.noreply.github.com>
2020-03-06 13:08:01 +0300

    init

---
+++ myApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Bennard brune Parker on 05/03/2020.
+//  Copyright © 2020 Solirize. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
